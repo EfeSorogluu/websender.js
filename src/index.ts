fba22b785b94f3cadf781c89a07e9720b783a782
Mert Efe SOROĞLU <sorolu14@gmail.com>
2025-07-09 15:26:20 +0300

    fixed ESM & CommonJS package problem

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { Websender } from './libs/websender.js';
-import type { WebsenderConfig, WebsenderResponse } from './types/websender.d.ts';
+import type { WebsenderConfig, WebsenderResponse } from './types/websender';
 
 export {
     Websender,
@@ -10,3 +10,10 @@ export {
 export default Websender;
 
 console.log('Use "npm run test" to test the library.');
+
+// CommonJS desteği için:
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+    module.exports = Websender;
+}
